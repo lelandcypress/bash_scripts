2fb735fe4627e7aacc7068c26d2d2c79876b974c
Michael Souter <souterm1@gmail.com>
2023-09-13 12:29:56 -0400

    sending finalized script to repository for review

--- move.sh
+++ move.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ##storing directory paths in variables to improve readability
-FILE1=Desktop/file1.txt
-FILE2=Desktop/file2.txt
-FILE3=Desktop/file3.txt
-FILE4=Desktop/file4.txt
+FILE1=~/Desktop/file1.txt
+FILE2=~/Desktop/file2.txt
+FILE3=~/Desktop/file3.txt
+FILE4=/~Desktop/file4.txt
 
 ##storing stage 2 (the swap back) in a function due to improve readability of code
 stage_two(){
     read -p "Do you wish to continue?" yn
     case $yn in 
         ['Yy']* ) 
-            mv $FILE2 $FILE1 mv $FILE4 $FILE3; break;;
+            mv $FILE2 $FILE1 && mv $FILE4 $FILE3;;
         ['Nn']* ) 
             echo "Exiting"
             exit ;; 
@@ -20,10 +20,9 @@ stage_two(){
     echo "Files swapped back" 
 }
 ##Script inits here, starts by verifying files exist, then executes first series of moves. 
-if [ -f "$FILE1" -a -f "$FILE3"]; 
+if [ -f "$FILE1" -a -f "$FILE3" ]; 
     then 
-        mv $FILE1 $FILE2;
-        mv $FILE3 $FILE4;
+        mv $FILE1 $FILE2 && mv $FILE3 $FILE4;
         echo "Files Swapped";  
-        stage_two();
+        stage_two;
 fi
